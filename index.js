f401b501afbc16bd39a3f3e8a855650c4cff8f14
Ramon Balthazar <ramon@moblee.net>
2018-03-15 20:24:39 -0300

    Renames lib

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const ICD = {
+const IMLG = {
   INTERVAL_TIME_MS: 100,
 
   FETCH_TYPES: {
@@ -10,50 +10,50 @@ const ICD = {
 
   scrollPosition: 0,
   scroll: function() {
-    ICD.scrollPosition += 1000;
-    window.scrollTo(0, ICD.scrollPosition);
+    IMLG.scrollPosition += 1000;
+    window.scrollTo(0, IMLG.scrollPosition);
   },
 
   isPostTypeValid: function(node) {
-    const videoIcons = node.getElementsByClassName(ICD.VIDEO_POST_CLASSES);
+    const videoIcons = node.getElementsByClassName(IMLG.VIDEO_POST_CLASSES);
     const postType = videoIcons.length ? 'video' : 'photo';
-    return ICD.FETCH_TYPES[postType];
+    return IMLG.FETCH_TYPES[postType];
   },
 
   postLinks: [],
   updateLinks: function() {
-    const postElements = document.getElementsByClassName(ICD.POST_CLASSES);
+    const postElements = document.getElementsByClassName(IMLG.POST_CLASSES);
     const newLinks = Array.from(postElements)
-      .filter(ICD.isPostTypeValid)
+      .filter(IMLG.isPostTypeValid)
       .map(function(node) {
         const postLink = node.getElementsByTagName('a')[0].href;
         const cleanLink = postLink.split('?')[0];
         return cleanLink;
       });
-    ICD.postLinks = ICD.postLinks.concat(newLinks);
+    IMLG.postLinks = IMLG.postLinks.concat(newLinks);
   },
 
   removeDuplicates: function() {
-    const uniqueLinks = new Set(ICD.postLinks);
-    ICD.postLinks = [...uniqueLinks];
+    const uniqueLinks = new Set(IMLG.postLinks);
+    IMLG.postLinks = [...uniqueLinks];
   },
 
   loadPosts: function() {
-    ICD.scroll();
-    ICD.updateLinks();
-    ICD.removeDuplicates();
+    IMLG.scroll();
+    IMLG.updateLinks();
+    IMLG.removeDuplicates();
   },
 
   interval_id: null,
   interval_active: false,
   start: function() {
-    ICD.interval_active = true;
-    ICD.interval_id = ICD.setIntervalMin(ICD.loadPosts, ICD.INTERVAL_TIME_MS);
+    IMLG.interval_active = true;
+    IMLG.interval_id = IMLG.setIntervalMin(IMLG.loadPosts, IMLG.INTERVAL_TIME_MS);
     return undefined;
   },
 
   setIntervalMin: function(callback, delay) {
-    if (!ICD.interval_active) {
+    if (!IMLG.interval_active) {
       return undefined;
     }
     const timeBefore = performance.now();
@@ -62,21 +62,20 @@ const ICD = {
 
     const remainingTime = delay - (performance.now() - timeBefore);
     setTimeout(function() {
-      ICD.setIntervalMin(callback, delay);
+      IMLG.setIntervalMin(callback, delay);
     }, remainingTime);
   },
 
   stopScrolling: function() {
-    ICD.interval_active = false;
+    IMLG.interval_active = false;
   },
 
   finish: function() {
-    ICD.stopScrolling();
-    ICD.removeDuplicates();
-    window.copy(ICD.postLinks.join('\n'));
+    IMLG.stopScrolling();
+    IMLG.removeDuplicates();
+    window.copy(IMLG.postLinks.join('\n'));
     console.log("Copied all posts' links to clipboard!");
   },
 };
 
-ICD.start();
-ICD.finish();
+IMLG.start();
